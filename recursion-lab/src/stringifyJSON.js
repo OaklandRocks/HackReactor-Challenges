dd7fc8e2860a3d7992747a332ac48eb7b7765d03
Peter Brown <peterbrownpa@gmail.com>
2016-03-29 15:35:31 -0700

    started need to work on stringifying objects with for in loop

--- recursion-lab/src/stringifyJSON.js
+++ recursion-lab/src/stringifyJSON.js
@@ -4,5 +4,26 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (typeof obj === 'boolean') {
+    return '' + obj;
+  }
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if(Array.isArray(obj) && (obj.length === 0)){
+      return '[]';
+  } else {
+    var results =[]
+    for (var i=0; i<obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+    }
+    return "[" + results.join(',') + "]" ;
+  }
 };
